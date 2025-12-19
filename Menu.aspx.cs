b96dc06b470ac346763dc09037b453863c241f1d
RajatSahu <sahurajat097@gmail.com>
2025-12-19 12:31:47 +0530

    Add files via upload

---
+++ Menu.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Menu : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btninsert_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Insert.aspx");
+    }
+
+    protected void btnview_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("view.aspx");
+    }
+
+    protected void btnupdate_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Update.aspx");
+    }   
+
+    protected void btndelete_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Delete.aspx");
+    }
+}
